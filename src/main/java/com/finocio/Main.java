dd67079bd32b7ba0a11a0ccea07aa81bfacc6a90
Jose <practicante3finocio@gmail.com>
2025-12-09 12:28:40 +0100

    feature: read args

--- src/main/java/com/finocio/Main.java
+++ src/main/java/com/finocio/Main.java
@@ -3,6 +3,11 @@ package com.finocio;
 public class Main {
     public static void main(String[] args) {
         //read arguments
-        System.out.println("Hello world!");
+
+        String a = args[0];
+        String b = args[1];
+        String c = args[2];
+
+        System.out.println("a= " + a + " b= " + b +" c=" + c);
     }
 }
