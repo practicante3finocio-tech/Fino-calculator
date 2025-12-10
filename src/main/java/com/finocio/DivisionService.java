492ffe1aef0932edaea0c71e8260dbbc26f3841c
Jose <practicante3finocio@gmail.com>
2025-12-10 10:49:13 +0100

    feature: create class DivisionService

---
+++ src/main/java/com/finocio/DivisionService.java
@@ -0,0 +1,29 @@
+package com.finocio;
+
+public class DivisionService implements CalculatorService{
+
+    @Override
+    public double add(double a, double b) {
+        throw new UnsupportedOperationException("Add not supported");
+    }
+
+    @Override
+    public double subtract(double a, double b) {
+        throw new UnsupportedOperationException("Subtract not supported");
+    }
+
+    @Override
+    public double multiply(double a, double b) {
+        throw new UnsupportedOperationException("Multiply not supported");
+    }
+
+    @Override
+    public double divide(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("El denominador no puede ser cero.");
+        }
+        return a / b;
+    }
+
+
+}
