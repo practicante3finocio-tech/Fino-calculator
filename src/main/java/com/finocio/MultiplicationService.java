625b3d45c41c72347326acca675f6f70b94a5b16
practicante1finocio@gmail.com <practicante1finocio@gmail.com>
2025-12-10 12:17:32 +0100

    feature: code-comments-multiply-implementation

--- src/main/java/com/finocio/MultiplicationService.java
+++ src/main/java/com/finocio/MultiplicationService.java
@@ -1,26 +1,47 @@
 package com.finocio;
 
+/**
+ * MultiplicationService provides the implementation of the "multiply" operation.
+ * All other operations are not supported in this service.
+ */
 public class MultiplicationService implements CalculatorService {
 
+    /**
+     * This service only supports multiplication.
+     * Calling add will throw an exception.
+     */
     @Override
     public double add(double a, double b) {
-        throw new UnsupportedOperationException("Add not supported");
+        throw new UnsupportedOperationException("Add not supported in MultiplicationService");
     }
 
+    /**
+     * This service only supports multiplication.
+     * Calling subtract will throw an exception.
+     */
     @Override
     public double subtract(double a, double b) {
-        throw new UnsupportedOperationException("Subtract not supported");
+        throw new UnsupportedOperationException("Subtract not supported in MultiplicationService");
     }
 
-    //Metodo de la clase donde nos retorna la operacion de la multiplicacion
-
+    /**
+     * Performs multiplication between two numbers.
+     *
+     * @param a first number
+     * @param b second number
+     * @return the result of a * b
+     */
     @Override
     public double multiply(double a, double b) {
         return a * b;
     }
 
+    /**
+     * This service only supports multiplication.
+     * Calling divide will throw an exception.
+     */
     @Override
     public double divide(double a, double b) {
-        throw new UnsupportedOperationException("Divide not supported");
+        throw new UnsupportedOperationException("Divide not supported in MultiplicationService");
     }
 }
